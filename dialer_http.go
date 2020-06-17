ce878414eefc6097a7c5773dce41d60047060ec4
phuslu <phus.lu@gmail.com>
2020-06-17 16:08:48 +0800

    change default ua

--- dialer_http.go
+++ dialer_http.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	DefaultHTTPDialerUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+	DefaultHTTPDialerUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 )
 
 var (
