f1207fc7d17ea77cff4f2c378d69d102974af387
phuslu <phus.lu@gmail.com>
2020-08-20 21:15:25 +0800

    update to go1.15

--- build.bash
+++ build.bash
@@ -6,7 +6,7 @@ function setup() {
 	mkdir -p ~/.ssh
 	ssh-keyscan -H github.com | tee -a ~/.ssh/known_hosts
 
-	curl 'https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz' | tar xz -C /tmp/
+	curl 'https://dl.google.com/go/go1.15.linux-amd64.tar.gz' | tar xz -C /tmp/
 }
 
 function build() {
