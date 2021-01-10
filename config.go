4e8deb079f7aa676e63d838077aa29f890db496c
phuslu <phus.lu@gmail.com>
2021-01-10 23:29:24 +0800

    go1.16 struct tag

--- config.go
+++ config.go
@@ -11,112 +11,112 @@ import (
 )
 
 type HTTPConfig struct {
-	Listen         []string          `json:"listen" yaml:"listen"`
-	ServerName     []string          `json:"server_name" yaml:"server_name"`
-	Keyfile        string            `json:"keyfile" yaml:"keyfile"`
-	Certfile       string            `json:"certfile" yaml:"certfile"`
-	DisableHttp2   bool              `json:"disable_http2" yaml:"disable_http2"`
-	PreferChacha20 bool              `json:"perfer_chacha20" yaml:"perfer_chacha20"`
-	Mimes          map[string]string `json:"mimes" yaml:"mimes"`
+	Listen         []string          `json yaml:"listen"`
+	ServerName     []string          `json yaml:"server_name"`
+	Keyfile        string            `json yaml:"keyfile"`
+	Certfile       string            `json yaml:"certfile"`
+	DisableHttp2   bool              `json yaml:"disable_http2"`
+	PreferChacha20 bool              `json yaml:"perfer_chacha20"`
+	Mimes          map[string]string `json yaml:"mimes"`
 	Forward        struct {
-		Policy       string   `json:"policy" yaml:"policy"`
-		Auth         string   `json:"auth" yaml:"auth"`
-		Upstream     string   `json:"upstream" yaml:"upstream"`
-		AllowDomains []string `json:"allow_domains" yaml:"allow_domains"`
-		DenyDomains  []string `json:"deny_domains" yaml:"deny_domains"`
-		SpeedLimit   int64    `json:"speed_limit" yaml:"speed_limit"`
-		OutboundIp   string   `json:"outbound_ip" yaml:"outbound_ip"`
-		Log          bool     `json:"log" yaml:"log"`
-	} `json:"forward" yaml:"forward"`
+		Policy       string   `json yaml:"policy"`
+		Auth         string   `json yaml:"auth"`
+		Upstream     string   `json yaml:"upstream"`
+		AllowDomains []string `json yaml:"allow_domains"`
+		DenyDomains  []string `json yaml:"deny_domains"`
+		SpeedLimit   int64    `json yaml:"speed_limit"`
+		OutboundIp   string   `json yaml:"outbound_ip"`
+		Log          bool     `json yaml:"log"`
+	} `json yaml:"forward"`
 	Web []struct {
-		Location string `json:"location" yaml:"location"`
+		Location string `json yaml:"location"`
 		Index    struct {
-			Root    string `json:"root" yaml:"root"`
-			Headers string `json:"headers" yaml:"headers"`
-			Body    string `json:"body" yaml:"body"`
-			Webdav  bool   `json:"webdav" yaml:"webdav"`
-		} `json:"index" yaml:"index"`
+			Root    string `json yaml:"root"`
+			Headers string `json yaml:"headers"`
+			Body    string `json yaml:"body"`
+			Webdav  bool   `json yaml:"webdav"`
+		} `json yaml:"index"`
 		Proxy struct {
-			Pass        string `json:"pass" yaml:"pass"`
-			SetHeaders  string `json:"set_headers" yaml:"set_headers"`
-			DumpFailure bool   `json:"dump_failure" yaml:"dump_failure"`
-		} `json:"proxy" yaml:"proxy"`
+			Pass        string `json yaml:"pass"`
+			SetHeaders  string `json yaml:"set_headers"`
+			DumpFailure bool   `json yaml:"dump_failure"`
+		} `json yaml:"proxy"`
 		Pprof struct {
-			Enabled bool `json:"enabled" yaml:"enabled"`
-		} `json:"pprof" yaml:"pprof"`
+			Enabled bool `json yaml:"enabled"`
+		} `json yaml:"pprof"`
 		Pac struct {
-			Enabled bool `json:"enabled" yaml:"enabled"`
-		} `json:"pac" yaml:"pac"`
+			Enabled bool `json yaml:"enabled"`
+		} `json yaml:"pac"`
 		Doh struct {
-			Enabled  bool                `json:"enabled" yaml:"enabled"`
-			Upstream string              `json:"upstream" yaml:"upstream"`
-			Prelude  map[string][]string `json:"prelude" yaml:"prelude"`
-		} `json:"doh" yaml:"doh"`
-	} `json:"web" yaml:"web"`
+			Enabled  bool                `json yaml:"enabled"`
+			Upstream string              `json yaml:"upstream"`
+			Prelude  map[string][]string `json yaml:"prelude"`
+		} `json yaml:"doh"`
+	} `json yaml:"web"`
 }
 
 type SocksConfig struct {
-	Listen  []string `json:"listen" yaml:"listen"`
+	Listen  []string `json yaml:"listen"`
 	Forward struct {
-		Policy       string   `json:"policy" yaml:"policy"`
-		Auth         string   `json:"auth" yaml:"auth"`
-		Upstream     string   `json:"upstream" yaml:"upstream"`
-		AllowDomains []string `json:"allow_domain" yaml:"allow_domain"`
-		DenyDomains  []string `json:"deny_domains" yaml:"deny_domains"`
-		SpeedLimit   int64    `json:"speed_limit" yaml:"speed_limit"`
-		OutboundIp   string   `json:"outbound_ip" yaml:"outbound_ip"`
-		Log          bool     `json:"log" yaml:"log"`
-	} `json:"forward" yaml:"forward"`
+		Policy       string   `json yaml:"policy"`
+		Auth         string   `json yaml:"auth"`
+		Upstream     string   `json yaml:"upstream"`
+		AllowDomains []string `json yaml:"allow_domain"`
+		DenyDomains  []string `json yaml:"deny_domains"`
+		SpeedLimit   int64    `json yaml:"speed_limit"`
+		OutboundIp   string   `json yaml:"outbound_ip"`
+		Log          bool     `json yaml:"log"`
+	} `json yaml:"forward"`
 }
 
 type RelayConfig struct {
-	Listen     []string `json:"listen" yaml:"listen"`
-	To         string   `json:"to" yaml:"to"`
-	Upstream   string   `json:"upstream" yaml:"upstream"`
-	SpeedLimit int64    `json:"speed_limit" yaml:"speed_limit"`
-	Log        bool     `json:"log" yaml:"log"`
+	Listen     []string `json yaml:"listen"`
+	To         string   `json yaml:"to"`
+	Upstream   string   `json yaml:"upstream"`
+	SpeedLimit int64    `json yaml:"speed_limit"`
+	Log        bool     `json yaml:"log"`
 }
 
 type DNSConfig struct {
-	Listen   []string `json:"listen" yaml:"listen"`
-	Upstream []string `json:"upstream" yaml:"upstream"`
+	Listen   []string `json yaml:"listen"`
+	Upstream []string `json yaml:"upstream"`
 }
 
 type Config struct {
 	Log struct {
-		Level     string `json:"level" yaml:"level"`
-		Backups   int    `json:"backups" yaml:"backups"`
-		Maxsize   int64  `json:"maxsize" yaml:"maxsize"`
-		Localtime bool   `json:"localtime" yaml:"localtime"`
-	} `json:"log" yaml:"log"`
+		Level     string `json yaml:"level"`
+		Backups   int    `json yaml:"backups"`
+		Maxsize   int64  `json yaml:"maxsize"`
+		Localtime bool   `json yaml:"localtime"`
+	} `json yaml:"log"`
 	Global struct {
-		DenyIntranet    bool   `json:"deny_intranet" yaml:"deny_intranet"`
-		DialTimeout     int    `json:"dial_timeout" yaml:"dial_timeout"`
-		TcpFastopen     bool   `json:"tcp_fastopen" yaml:"tcp_fastopen"`
-		PreferIpv6      bool   `json:"perfer_ipv6" yaml:"perfer_ipv6"`
-		DnsServer       string `json:"dns_server" yaml:"dns_server"`
-		DnsTtl          uint32 `json:"dns_ttl" yaml:"dns_ttl"`
-		IdleConnTimeout int    `json:"idle_conn_timeout" yaml:"idle_conn_timeout"`
-		MaxIdleConns    int    `json:"max_idle_conns" yaml:"max_idle_conns"`
-		GracefulTimeout int    `json:"graceful_timeout" yaml:"graceful_timeout"`
-	} `json:"global" yaml:"global"`
+		DenyIntranet    bool   `json yaml:"deny_intranet"`
+		DialTimeout     int    `json yaml:"dial_timeout"`
+		TcpFastopen     bool   `json yaml:"tcp_fastopen"`
+		PreferIpv6      bool   `json yaml:"perfer_ipv6"`
+		DnsServer       string `json yaml:"dns_server"`
+		DnsTtl          uint32 `json yaml:"dns_ttl"`
+		IdleConnTimeout int    `json yaml:"idle_conn_timeout"`
+		MaxIdleConns    int    `json yaml:"max_idle_conns"`
+		GracefulTimeout int    `json yaml:"graceful_timeout"`
+	} `json yaml:"global"`
 	Cron []struct {
-		Spec    string `json:"spec" yaml:"spec"`
-		Command string `json:"command" yaml:"command"`
-	} `json:"cron" yaml:"cron"`
-	Https    []HTTPConfig  `json:"https" yaml:"https"`
-	Http     []HTTPConfig  `json:"http" yaml:"http"`
-	Socks    []SocksConfig `json:"socks" yaml:"socks"`
-	Relay    []RelayConfig `json:"relay" yaml:"relay"`
-	Dns      []DNSConfig   `json:"dns" yaml:"dns"`
+		Spec    string `json yaml:"spec"`
+		Command string `json yaml:"command"`
+	} `json yaml:"cron"`
+	Https    []HTTPConfig  `json yaml:"https"`
+	Http     []HTTPConfig  `json yaml:"http"`
+	Socks    []SocksConfig `json yaml:"socks"`
+	Relay    []RelayConfig `json yaml:"relay"`
+	Dns      []DNSConfig   `json yaml:"dns"`
 	Upstream map[string]struct {
-		Scheme    string `json:"scheme" yaml:"scheme"`
-		Username  string `json:"username" yaml:"username"`
-		Password  string `json:"password" yaml:"password"`
-		Host      string `json:"host" yaml:"host"`
-		Port      int    `json:"port" yaml:"port"`
-		UserAgent string `json:"user_agent" yaml:"user_agent"`
-	} `json:"upstream" yaml:"upstream"`
+		Scheme    string `json yaml:"scheme"`
+		Username  string `json yaml:"username"`
+		Password  string `json yaml:"password"`
+		Host      string `json yaml:"host"`
+		Port      int    `json yaml:"port"`
+		UserAgent string `json yaml:"user_agent"`
+	} `json yaml:"upstream"`
 }
 
 func NewConfig(filename string) (*Config, error) {
