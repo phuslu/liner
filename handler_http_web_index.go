19a277c9ec7b9ac05edf4c3bf591f81ef3b16ae7
phuslu <phus.lu@gmail.com>
2021-01-11 01:01:48 +0800

    add headers to web index request

--- handler_http_web_index.go
+++ handler_http_web_index.go
@@ -65,6 +65,8 @@ func (h *HTTPWebIndexHandler) Load() (err error) {
 func (h *HTTPWebIndexHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	ri := req.Context().Value(RequestInfoContextKey).(*RequestInfo)
 
+	log.Debug().Context(ri.LogContext).Interface("headers", req.Header).Msg("web index request")
+
 	if h.Root == "" && h.Body == "" {
 		http.NotFound(rw, req)
 		return
