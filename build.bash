6769fd837335351b31d847b6034035f2c68ea922
phuslu <phus.lu@gmail.com>
2021-06-08 23:57:51 +0800

    add checksums.txt

--- build.bash
+++ build.bash
@@ -128,10 +128,12 @@ EOF
 function release() {
 	pushd build
 
+	sha1sum liner_* >checksums.txt
+
 	local ssh_host=phus.lu
 	ssh-keyscan -H ${ssh_host} | tee -a ~/.ssh/known_hosts
 	sshpass -p "${SSH_PASSWORD}" ssh phuslu@${ssh_host} 'rm -rf /home/phuslu/web/liner/liner_*'
-	sshpass -p "${SSH_PASSWORD}" rsync --progress -avz liner_* "phuslu@${ssh_host}:/home/phuslu/web/liner/"
+	sshpass -p "${SSH_PASSWORD}" rsync --progress -avz liner_* checksums.txt "phuslu@${ssh_host}:/home/phuslu/web/liner/"
 
 	popd
 }
