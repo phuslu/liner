d05e02d71754912bb434746578791b01144c45fe
phuslu <phus.lu@gmail.com>
2023-09-29 10:33:31 +0800

    fix IsReservedIP for nil ip

--- helpers.go
+++ helpers.go
@@ -502,6 +502,9 @@ func IsTimeout(err error) bool {
 
 // see https://en.wikipedia.org/wiki/Reserved_IP_addresses
 func IsReservedIP(ip net.IP) bool {
+	if ip == nil {
+		return false
+	}
 	if ip4 := ip.To4(); ip4 != nil {
 		switch ip4[0] {
 		case 10:
