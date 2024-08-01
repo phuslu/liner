bb9f6adb1994abb86e8af58669a161d4f7116d7c
phuslu <phus.lu@gmail.com>
2024-08-01 18:44:48 +0800

    remove unneeded functions

--- functions.go
+++ functions.go
@@ -7,7 +7,6 @@ import (
 	"net"
 	"os"
 	"path/filepath"
-	"reflect"
 	"regexp"
 	"strings"
 	"text/template"
@@ -275,57 +274,4 @@ func (f *Functions) regexMatch(pattern, s string) bool {
 		return false
 	}
 	return regex.MatchString(s)
-}
-
-func (f *Functions) empty(given interface{}) bool {
-	g := reflect.ValueOf(given)
-	if !g.IsValid() {
-		return true
-	}
-
-	// Basically adapted from text/template.isTrue
-	switch g.Kind() {
-	default:
-		return g.IsNil()
-	case reflect.Array, reflect.Slice, reflect.Map, reflect.String:
-		return g.Len() == 0
-	case reflect.Bool:
-		return !g.Bool()
-	case reflect.Complex64, reflect.Complex128:
-		return g.Complex() == 0
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return g.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return g.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return g.Float() == 0
-	case reflect.Struct:
-		return false
-	}
-}
-
-func (f *Functions) all(v ...interface{}) bool {
-	for _, val := range v {
-		if f.empty(val) {
-			return false
-		}
-	}
-	return true
-}
-
-func (f *Functions) any(v ...interface{}) bool {
-	for _, val := range v {
-		if !f.empty(val) {
-			return true
-		}
-	}
-	return false
-}
-
-func (f *Functions) ternary(vt interface{}, vf interface{}, v bool) interface{} {
-	if v {
-		return vt
-	}
-
-	return vf
 }
