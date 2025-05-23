47f6d2017453d9a387a0a5f798fc7b43fe1f9180
phuslu <phus.lu@gmail.com>
2025-05-23 19:59:11 +0800

    set io_copy_buffer to get.sh

--- get.sh
+++ get.sh
@@ -46,6 +46,15 @@ if test -s .env; then
   exit 0
 fi
 
+if test -f /sys/fs/cgroup/memory.max; then
+  io_copy_buffer=$(($(</sys/fs/cgroup/memory.max) / 8192))
+else
+  io_copy_buffer=$(($(awk '/MemTotal/ {print $2 * 1024}' /proc/meminfo) / 8192))
+fi
+if [ "$io_copy_buffer" -gt 524288 ]; fi
+  io_copy_buffer=524288
+fi
+
 cat <<EOF > production.yaml
 global:
   log_level: info
@@ -54,10 +63,11 @@ global:
   dns_cache_duration: 15m
 https:
   - listen: [':443']
-    server_name: ['$domain']
+    server_name: ['${domain}']
     forward:
       log: true
       prefer_ipv6: false
+      io_copy_buffer: ${io_copy_buffer}
       policy: |
         {{if all (.Request.ProtoAtLeast 2 0) (eq .Request.TLS.Version 0x0304) (greased .ClientHelloInfo)}}
             bypass_auth
@@ -65,7 +75,7 @@ https:
             proxy_pass
         {{end}}
     web:
-      - location: /$pacfile
+      - location: /${pacfile}
         index:
           headers: "content-type: text/plain;charset=utf-8"
           file: $(pwd)/china.pac
