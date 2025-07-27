cf7123240856d5e9264b42502c2bf9d864489a27
phuslu <phus.lu@gmail.com>
2025-07-27 16:58:32 +0800

    tweak IsReservedIP

--- helpers.go
+++ helpers.go
@@ -1036,45 +1036,44 @@ func PlainAddrFromUDPAddr(na *net.UDPAddr) (addr PlainAddr) {
 }
 
 // see https://en.wikipedia.org/wiki/Reserved_IP_addresses
-func IsReservedIP(ip net.IP) bool {
-	if ip == nil {
+func IsReservedIP(ip netip.Addr) bool {
+	if !ip.Is4() {
 		return false
 	}
-	if ip4 := ip.To4(); ip4 != nil {
-		switch ip4[0] {
-		case 10:
-			return true
-		case 100:
-			return ip4[1] >= 64 && ip4[1] <= 127
-		case 127:
-			return true
-		case 169:
-			return ip4[1] == 254
-		case 172:
-			return ip4[1] >= 16 && ip4[1] <= 31
-		case 192:
-			switch ip4[1] {
-			case 0:
-				switch ip4[2] {
-				case 0, 2:
-					return true
-				}
-			case 18, 19:
-				return true
-			case 51:
-				return ip4[2] == 100
-			case 88:
-				return ip4[2] == 99
-			case 168:
+	b := ip.As4()
+	switch b[0] {
+	case 10:
+		return true
+	case 100:
+		return b[1] >= 64 && b[1] <= 127
+	case 127:
+		return true
+	case 169:
+		return b[1] == 254
+	case 172:
+		return b[1] >= 16 && b[1] <= 31
+	case 192:
+		switch b[1] {
+		case 0:
+			switch b[2] {
+			case 0, 2:
 				return true
 			}
-		case 203:
-			return ip4[1] == 0 && ip4[2] == 113
-		case 224:
+		case 18, 19:
 			return true
-		case 240:
+		case 51:
+			return b[2] == 100
+		case 88:
+			return b[2] == 99
+		case 168:
 			return true
 		}
+	case 203:
+		return b[1] == 0 && b[2] == 113
+	case 224:
+		return true
+	case 240:
+		return true
 	}
 	return false
 }
