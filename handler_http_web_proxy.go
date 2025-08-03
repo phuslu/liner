668708422e72e83ffb9156fde0c2dac9425a7a12
phuslu <phus.lu@gmail.com>
2025-08-03 22:11:24 +0800

    dont set x-ja4

--- handler_http_web_proxy.go
+++ handler_http_web_proxy.go
@@ -228,7 +228,7 @@ func (h *HTTPWebProxyHandler) ServeHTTP(rw http.ResponseWriter, req *http.Reques
 		// req.Header.Set("x-forwarded-ssl", "on")
 		// req.Header.Set("x-url-scheme", "https")
 		// req.Header.Set("x-http-proto", req.Proto)
-		req.Header.Set("x-ja4", string(ri.JA4))
+		// req.Header.Set("x-ja4", string(ri.JA4))
 	}
 	h.setHeaders(req, ri)
 
