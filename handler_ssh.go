e69bd6faa70f794406e71d7dd9edfe969cd3c968
phuslu <phus.lu@gmail.com>
2025-08-05 01:19:15 +0800

    tweak ssh handler

--- handler_ssh.go
+++ handler_ssh.go
@@ -202,197 +202,196 @@ func (h *SshHandler) handleConn(ctx context.Context, netConn net.Conn) {
 }
 
 func (h *SshHandler) handleChannels(ctx context.Context, chans <-chan ssh.NewChannel, conn *ssh.ServerConn) {
-	// Service the incoming Channel channel in go routine
 	for newChannel := range chans {
-		go h.handleChannel(ctx, newChannel, conn)
+		switch newChannel.ChannelType() {
+		case "session":
+			go func(ctx context.Context, newChannel ssh.NewChannel, conn *ssh.ServerConn) {
+				channel, requests, err := newChannel.Accept()
+				if err != nil {
+					h.Logger.Printf("Could not accept channel (%s)", err)
+					return
+				}
+				// Sessions have out-of-band requests such as "shell", "pty-req" and "env"
+				go h.handleSession(ctx, channel, requests, conn)
+			}(ctx, newChannel, conn)
+		case "direct-tcpip":
+			fallthrough
+		case "forwarded-tcpip":
+			fallthrough
+		case "x11":
+			fallthrough
+		default:
+			go newChannel.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", newChannel.ChannelType()))
+		}
 	}
 }
 
-func (h *SshHandler) handleChannel(ctx context.Context, newChannel ssh.NewChannel, conn *ssh.ServerConn) {
-	// Since we're handling a shell, we expect a
-	// channel type of "session". The also describes
-	// "x11", "direct-tcpip" and "forwarded-tcpip"
-	// channel types.
-	if t := newChannel.ChannelType(); t != "session" {
-		newChannel.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", t))
-		return
-	}
-
-	// At this point, we have the opportunity to reject the client's
-	// request for another logical channel
-	channel, requests, err := newChannel.Accept()
-	if err != nil {
-		h.Logger.Printf("Could not accept channel (%s)", err)
-		return
-	}
-
+func (h *SshHandler) handleSession(ctx context.Context, channel ssh.Channel, requests <-chan *ssh.Request, conn *ssh.ServerConn) {
 	// Sessions have out-of-band requests such as "shell", "pty-req" and "env"
-	go func() {
-		var shellfile *os.File
-		var width, height uint32
-		envs := map[string]string{}
+	var shellfile *os.File
+	var width, height uint32
+	envs := map[string]string{}
 
-		for req := range requests {
-			h.Logger.Info().NetAddr("remote_addr", conn.RemoteAddr()).Any("request", req).Msg("process ssh channel request")
-			switch req.Type {
-			case "exec":
-				req.Reply(true, nil)
+	for req := range requests {
+		h.Logger.Info().NetAddr("remote_addr", conn.RemoteAddr()).Any("request", req).Msg("process ssh channel request")
+		switch req.Type {
+		case "exec":
+			req.Reply(true, nil)
 
-				length := int(binary.BigEndian.Uint32(req.Payload))
-				command := string(req.Payload[4:])
-				if len(command) != length {
-					h.Logger.Fatal().Msgf("command length unmatch, got=%d, want=%d", len(command), length)
+			length := int(binary.BigEndian.Uint32(req.Payload))
+			command := string(req.Payload[4:])
+			if len(command) != length {
+				h.Logger.Fatal().Msgf("command length unmatch, got=%d, want=%d", len(command), length)
+			}
+
+			shellcmd := exec.CommandContext(ctx, h.shellPath, "-c", command)
+
+			var err error
+			var in io.WriteCloser
+			var out io.ReadCloser
+
+			// Prepare teardown function
+			close := func() {
+				in.Close()
+
+				err := shellcmd.Wait()
+				var exitStatus int32
+				if err != nil {
+					if e2, ok := err.(*exec.ExitError); ok {
+						if s, ok := e2.Sys().(syscall.WaitStatus); ok {
+							exitStatus = int32(s.ExitStatus())
+						} else {
+							panic(errors.New("unimplemented for system where exec.ExitError.Sys() is not syscall.WaitStatus"))
+						}
+					}
 				}
+				var b bytes.Buffer
+				binary.Write(&b, binary.BigEndian, exitStatus)
+				channel.SendRequest("exit-status", false, b.Bytes())
+				channel.Close()
+				h.Logger.Printf("Session closed")
+			}
 
-				shellcmd := exec.CommandContext(ctx, h.shellPath, "-c", command)
+			in, err = shellcmd.StdinPipe()
+			if err != nil {
+				h.Logger.Printf("Could not get stdin pipe (%s)", err)
+				close()
+				return
+			}
+
+			out, err = shellcmd.StdoutPipe()
+			if err != nil {
+				h.Logger.Printf("Could not get stdout pipe (%s)", err)
+				close()
+				return
+			}
+
+			err = shellcmd.Start()
+			if err != nil {
+				h.Logger.Printf("Could not start pty (%s)", err)
+				close()
+				return
+			}
+
+			//pipe session to shell and visa-versa
+			var once sync.Once
+			go func() {
+				io.Copy(channel, out)
+				once.Do(close)
+			}()
+			go func() {
+				io.Copy(in, channel)
+				once.Do(close)
+			}()
+		case "env":
+			if len(req.Payload) != 0 {
+				if len(req.Payload) < 8 {
+					h.Logger.Warn().Msg("Invalid env request payload")
+					req.Reply(false, nil)
+					continue
+				}
+				keylen := binary.BigEndian.Uint32(req.Payload[0:4])
+				if len(req.Payload) < 4+int(keylen)+4 {
+					h.Logger.Warn().Msg("Invalid env request payload")
+					req.Reply(false, nil)
+					continue
+				}
+				key := string(req.Payload[4 : 4+keylen])
+				valuelen := binary.BigEndian.Uint32(req.Payload[4+keylen:])
+				value := string(req.Payload[4+keylen+4 : 4+keylen+4+valuelen])
+				envs[key] = value
+				h.Logger.Info().Str("req_type", req.Type).Str("key", key).Str("value", value).Msg("handle ssh request")
+			}
+			req.Reply(true, nil)
+		case "shell":
+			// We only accept the default shell
+			// (i.e. no command in the Payload)
+			if len(req.Payload) == 0 {
+				req.Reply(true, nil)
 
 				var err error
-				var in io.WriteCloser
-				var out io.ReadCloser
-
-				// Prepare teardown function
-				close := func() {
-					in.Close()
-
-					err := shellcmd.Wait()
-					var exitStatus int32
-					if err != nil {
-						if e2, ok := err.(*exec.ExitError); ok {
-							if s, ok := e2.Sys().(syscall.WaitStatus); ok {
-								exitStatus = int32(s.ExitStatus())
-							} else {
-								panic(errors.New("unimplemented for system where exec.ExitError.Sys() is not syscall.WaitStatus"))
-							}
-						}
-					}
-					var b bytes.Buffer
-					binary.Write(&b, binary.BigEndian, exitStatus)
-					channel.SendRequest("exit-status", false, b.Bytes())
-					channel.Close()
-					h.Logger.Printf("Session closed")
-				}
-
-				in, err = shellcmd.StdinPipe()
+				// Fire up bash for this session
+				shellfile, err = h.startShell(ctx, h.shellPath, envs, channel)
 				if err != nil {
-					h.Logger.Printf("Could not get stdin pipe (%s)", err)
-					close()
-					return
+					h.Logger.Error().Err(err).Str("req_type", req.Type).Str("shell", h.shellPath).Any("envs", envs).Msg("handle ssh request")
+					req.Reply(false, nil)
 				}
 
-				out, err = shellcmd.StdoutPipe()
-				if err != nil {
-					h.Logger.Printf("Could not get stdout pipe (%s)", err)
-					close()
-					return
-				}
-
-				err = shellcmd.Start()
-				if err != nil {
-					h.Logger.Printf("Could not start pty (%s)", err)
-					close()
-					return
-				}
-
-				//pipe session to shell and visa-versa
-				var once sync.Once
-				go func() {
-					io.Copy(channel, out)
-					once.Do(close)
-				}()
-				go func() {
-					io.Copy(in, channel)
-					once.Do(close)
-				}()
-			case "env":
-				if len(req.Payload) != 0 {
-					if len(req.Payload) < 8 {
-						h.Logger.Warn().Msg("Invalid env request payload")
-						req.Reply(false, nil)
-						continue
-					}
-					keylen := binary.BigEndian.Uint32(req.Payload[0:4])
-					if len(req.Payload) < 4+int(keylen)+4 {
-						h.Logger.Warn().Msg("Invalid env request payload")
-						req.Reply(false, nil)
-						continue
-					}
-					key := string(req.Payload[4 : 4+keylen])
-					valuelen := binary.BigEndian.Uint32(req.Payload[4+keylen:])
-					value := string(req.Payload[4+keylen+4 : 4+keylen+4+valuelen])
-					envs[key] = value
-					h.Logger.Info().Str("req_type", req.Type).Str("key", key).Str("value", value).Msg("handle ssh request")
-				}
-				req.Reply(true, nil)
-			case "shell":
-				// We only accept the default shell
-				// (i.e. no command in the Payload)
-				if len(req.Payload) == 0 {
-					req.Reply(true, nil)
-
-					var err error
-					// Fire up bash for this session
-					shellfile, err = h.startShell(ctx, h.shellPath, envs, channel)
-					if err != nil {
-						h.Logger.Error().Err(err).Str("req_type", req.Type).Str("shell", h.shellPath).Any("envs", envs).Msg("handle ssh request")
-						req.Reply(false, nil)
-					}
-
-					// Set window size
-					if width > 0 && height > 0 {
-						SetTermWindowSize(shellfile.Fd(), uint16(width), uint16(height))
-						h.Logger.Info().Str("req_type", req.Type).Str("shell", h.shellPath).Uint32("width", width).Uint32("height", height).Msg("set term windows size")
-					}
-				}
-			case "pty-req":
-				length := int(binary.BigEndian.Uint32(req.Payload))
-				width = binary.BigEndian.Uint32(req.Payload[length+4:])
-				height = binary.BigEndian.Uint32(req.Payload[length+8:])
-				h.Logger.Info().Str("req_type", req.Type).Uint32("width", width).Uint32("height", height).Msg("handle ssh request")
-				if shellfile != nil {
+				// Set window size
+				if width > 0 && height > 0 {
 					SetTermWindowSize(shellfile.Fd(), uint16(width), uint16(height))
+					h.Logger.Info().Str("req_type", req.Type).Str("shell", h.shellPath).Uint32("width", width).Uint32("height", height).Msg("set term windows size")
 				}
-				// Responding true (OK) here will let the client
-				// know we have a pty ready for input
-				req.Reply(true, nil)
-			case "window-change":
-				width = binary.BigEndian.Uint32(req.Payload[0:])
-				height = binary.BigEndian.Uint32(req.Payload[4:])
-				h.Logger.Info().Str("req_type", req.Type).Uint32("width", width).Uint32("height", height).Msg("handle ssh request")
-				if shellfile != nil {
-					SetTermWindowSize(shellfile.Fd(), uint16(width), uint16(height))
-				}
-			case "subsystem":
-				length := int(binary.BigEndian.Uint32(req.Payload))
-				subsystem := b2s(req.Payload[4 : 4+length])
-				switch subsystem {
-				case "sftp", "internal-sftp":
-					h.Logger.Printf("sftp server serving: %s", req.Payload)
-					go func() {
-						defer channel.Close()
-						server, err := sftp.NewServer(channel)
-						if err != nil {
-							h.Logger.Printf("could not start sftp server: %s", err)
-							return
-						}
-						if err := server.Serve(); err == io.EOF {
-							server.Close()
-							h.Logger.Printf("sftp client exited session.")
-						} else if err != nil {
-							h.Logger.Printf("sftp server exited with error: %s", err)
-						}
-					}()
-					req.Reply(true, nil)
-				default:
-					h.Logger.Printf("ssh subsystem request %#v not supportted", req)
-					req.Reply(false, append([]byte("ssh subsystem is not supportted: "), req.Payload...))
-				}
-			case "keepalive@openssh.com":
+			}
+		case "pty-req":
+			length := int(binary.BigEndian.Uint32(req.Payload))
+			width = binary.BigEndian.Uint32(req.Payload[length+4:])
+			height = binary.BigEndian.Uint32(req.Payload[length+8:])
+			h.Logger.Info().Str("req_type", req.Type).Uint32("width", width).Uint32("height", height).Msg("handle ssh request")
+			if shellfile != nil {
+				SetTermWindowSize(shellfile.Fd(), uint16(width), uint16(height))
+			}
+			// Responding true (OK) here will let the client
+			// know we have a pty ready for input
+			req.Reply(true, nil)
+		case "window-change":
+			width = binary.BigEndian.Uint32(req.Payload[0:])
+			height = binary.BigEndian.Uint32(req.Payload[4:])
+			h.Logger.Info().Str("req_type", req.Type).Uint32("width", width).Uint32("height", height).Msg("handle ssh request")
+			if shellfile != nil {
+				SetTermWindowSize(shellfile.Fd(), uint16(width), uint16(height))
+			}
+		case "subsystem":
+			length := int(binary.BigEndian.Uint32(req.Payload))
+			subsystem := b2s(req.Payload[4 : 4+length])
+			switch subsystem {
+			case "sftp", "internal-sftp":
+				h.Logger.Printf("sftp server serving: %s", req.Payload)
+				go func() {
+					defer channel.Close()
+					server, err := sftp.NewServer(channel)
+					if err != nil {
+						h.Logger.Printf("could not start sftp server: %s", err)
+						return
+					}
+					if err := server.Serve(); err == io.EOF {
+						server.Close()
+						h.Logger.Printf("sftp client exited session.")
+					} else if err != nil {
+						h.Logger.Printf("sftp server exited with error: %s", err)
+					}
+				}()
 				req.Reply(true, nil)
 			default:
-				req.Reply(false, nil)
+				h.Logger.Printf("ssh subsystem request %#v not supportted", req)
+				req.Reply(false, append([]byte("ssh subsystem is not supportted: "), req.Payload...))
 			}
+		case "keepalive@openssh.com":
+			req.Reply(true, nil)
+		default:
+			req.Reply(false, nil)
 		}
-	}()
+	}
 }
 
 func (h *SshHandler) startShell(ctx context.Context, shellPath string, envs map[string]string, channel ssh.Channel) (*os.File, error) {
