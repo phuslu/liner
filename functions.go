2c6e5fe70b89edd0e942e709e9a9d749f501dd58
Benson Yan <fuxin04@gmail.com>
2025-12-12 10:46:37 +0800

    opimize: slog template support struct print

--- functions.go
+++ functions.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -18,7 +19,7 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/go-task/slim-sprig/v3"
+	sprig "github.com/go-task/slim-sprig/v3"
 	"github.com/phuslu/log"
 	"github.com/phuslu/lru"
 	"github.com/puzpuzpuz/xsync/v4"
@@ -89,6 +90,17 @@ func (f *Functions) Load() error {
 }
 
 func (f *Functions) slog(msg string, args ...any) string {
+	for i := 1; i < len(args); i += 2 {
+		switch args[i].(type) {
+		case string, bool, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
+		default:
+			if b, err := json.Marshal(args[i]); err == nil {
+				args[i] = string(b)
+			} else {
+				args[i] = fmt.Sprintf("%+v", args[i])
+			}
+		}
+	}
 	f.SlogLogger.Info(msg, args...)
 	return ""
 }
