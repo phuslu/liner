a7fa91c88678960d00b134dcbc8e7d8752a1b4ea
phuslu <phus.lu@gmail.com>
2026-03-05 14:18:38 +0800

    moved wheels to https://github.com/phuslu/liner/tree/python

--- build.bash
+++ build.bash
@@ -4,10 +4,10 @@ function setup() {
 	export DEBIAN_FRONTEND=noninteractive
 	if test -f /etc/alpine-release; then
 		apk update
-		apk add git curl jq zip xz github-cli patch make build-base py3-pip python3-dev patchelf upx
+		apk add git curl jq zip xz github-cli patch make build-base upx
 	else
 		apt update -y
-		apt install -yq git curl jq zip xz-utils gh build-essential python3-pip python3-venv python3-dev patchelf upx
+		apt install -yq git curl jq zip xz-utils gh build-essential upx
 	fi
 
 	git config --global --add safe.directory '*'
@@ -58,47 +58,14 @@ EOF
 
 }
 
-function wheel() {
-	export CGO_ENABLED=1
-	export GOROOT=${GOROOT:-/tmp/go}
-	export GOPATH=${GOPATH:-/tmp/gopath}
-	export PATH=${GOPATH:-~/go}/bin:${GOROOT}/bin:$PATH
-	export REVSION=$(git rev-list --count HEAD)
-
-	#go install -v mvdan.cc/garble@latest
-	if [ "$(uname)" = "Darwin" ]; then
-		if command -v garble; then
-			export GOGARBLE=liner
-		fi
-	fi
-
-	python3 setup.py bdist_wheel
-
-	if [ "$(uname)" = "Linux" ]; then
-		if python3 -c "import sys; exit(sys.prefix != getattr(sys, 'base_prefix', sys.prefix))"; then
-			python3 -m venv ~/.venv
-		fi
-		~/.venv/bin/pip install auditwheel
-		~/.venv/bin/auditwheel repair wheel/dist/liner*.whl
-	fi
-
-	mkdir -p build
-	mv wheelhouse/liner*.whl build/
-}
-
 function release() {
 	pushd build
 
-	if ls liner_*.whl; then
-		gh release view v0.0.0 --json assets --jq .assets[].name | egrep '^liner_py-' | grep "_$(arch).whl$" | xargs -i gh release delete-asset v0.0.0 {} --yes
-		gh release upload v0.0.0 liner_py-*.whl --clobber
-	elif ls liner_*; then
-		sha1sum liner* >checksums.txt
-		git log --oneline --pretty=format:"%h %s" -5 | tee changelog.txt
-		gh release view v0.0.0 --json assets --jq .assets[].name | egrep -v '^liner_py-' | xargs -i gh release delete-asset v0.0.0 {} --yes
-		gh release upload v0.0.0 liner_* checksums.txt --clobber
-		gh release edit v0.0.0 --notes-file changelog.txt
-	fi
+	sha1sum liner* >checksums.txt
+	git log --oneline --pretty=format:"%h %s" -5 | tee changelog.txt
+	gh release view v0.0.0 --json assets --jq .assets[].name | egrep -v '^liner_py-' | xargs -i gh release delete-asset v0.0.0 {} --yes
+	gh release upload v0.0.0 liner_* checksums.txt --clobber
+	gh release edit v0.0.0 --notes-file changelog.txt
 
 	popd
 }
