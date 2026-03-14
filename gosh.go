aeb2ee3b682d15d3d8223cd42e2b4ae2a26d09ac
phuslu <phus.lu@gmail.com>
2026-03-15 02:28:34 +0800

    support $- for gosh

--- gosh.go
+++ gosh.go
@@ -11,6 +11,7 @@ import (
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"reflect"
 	"runtime"
 	"slices"
 	"strconv"
@@ -20,8 +21,10 @@ import (
 	"time"
 	"unicode"
 	"unicode/utf8"
+	"unsafe"
 
 	"github.com/chzyer/readline"
+	"mvdan.cc/sh/v3/expand"
 	"mvdan.cc/sh/v3/interp"
 	"mvdan.cc/sh/v3/syntax"
 )
@@ -53,6 +56,7 @@ func gosh(ctx context.Context, isatty bool, stdin io.Reader, stdout, stderr io.W
 	if err != nil {
 		return err
 	}
+	goshInstallShellOptionVariable(runner, isatty, stdin != nil)
 
 	// source the init files.
 	if file, err := os.Open(cmp.Or(os.Getenv("GOSH_ENV"), os.ExpandEnv("$HOME/.profile"))); err == nil {
@@ -703,6 +707,101 @@ func (p *goshPromptState) escapeDouble(s string) string {
 	s = strings.ReplaceAll(s, "\\", "\\\\")
 	s = strings.ReplaceAll(s, "\"", "\\\"")
 	return s
+}
+
+func goshInstallShellOptionVariable(runner *interp.Runner, interactive, readFromStdin bool) {
+	if runner == nil {
+		return
+	}
+	base := runner.Env
+	if base == nil {
+		base = expand.ListEnviron()
+	}
+	provider := &goshShellOptionProvider{
+		runner:        runner,
+		interactive:   interactive,
+		readFromStdin: readFromStdin,
+	}
+	runner.Env = &goshShellEnviron{base: base, flags: provider.Flags}
+}
+
+type goshShellEnviron struct {
+	base  expand.Environ
+	flags func() string
+}
+
+func (e *goshShellEnviron) Get(name string) expand.Variable {
+	if name == "-" {
+		return expand.Variable{Set: true, Kind: expand.String, Str: e.flags()}
+	}
+	if e.base == nil {
+		return expand.Variable{}
+	}
+	return e.base.Get(name)
+}
+
+func (e *goshShellEnviron) Each(f func(name string, vr expand.Variable) bool) {
+	if e.base == nil {
+		return
+	}
+	e.base.Each(f)
+}
+
+type goshShellOptionProvider struct {
+	runner        *interp.Runner
+	interactive   bool
+	readFromStdin bool
+}
+
+func (p *goshShellOptionProvider) Flags() string {
+	if p == nil || p.runner == nil {
+		return ""
+	}
+	opts := goshRunnerOpts(p.runner)
+	var b strings.Builder
+	b.WriteByte('h')
+	if p.interactive {
+		b.WriteByte('i')
+		b.WriteByte('m')
+		b.WriteByte('B')
+		b.WriteByte('H')
+		b.WriteByte('s')
+	} else {
+		b.WriteByte('B')
+		if p.readFromStdin {
+			b.WriteByte('s')
+		}
+	}
+	// shell option mapping based on interp.shellOptsTable order
+	// (allexport, errexit, noexec, noglob, nounset, xtrace).
+	for _, opt := range []struct {
+		index int
+		flag  byte
+	}{
+		{0, 'a'},
+		{1, 'e'},
+		{2, 'n'},
+		{3, 'f'},
+		{4, 'u'},
+		{5, 'x'},
+	} {
+		if opt.index < len(opts) && opts[opt.index] {
+			b.WriteByte(opt.flag)
+		}
+	}
+	return b.String()
+}
+
+func goshRunnerOpts(r *interp.Runner) []bool {
+	if r == nil {
+		return nil
+	}
+	val := reflect.ValueOf(r).Elem().FieldByName("opts")
+	if !val.IsValid() || !val.CanAddr() || val.Len() == 0 {
+		return nil
+	}
+	ptr := unsafe.Pointer(val.UnsafeAddr())
+	return unsafe.Slice((*bool)(ptr), val.Len())
 }
 
 func goshBindExecMiddleware(mgr *goshKeyBindingManager) func(next interp.ExecHandlerFunc) interp.ExecHandlerFunc {
