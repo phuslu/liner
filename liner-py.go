8e3724e9e34e0cbf63e85586b42575adff54e0f0
phuslu <phus.lu@gmail.com>
2026-04-06 23:38:55 +0800

    merge liner-so.go to liner-py.go

--- liner-py.go
+++ liner-py.go
@@ -1,10 +1,18 @@
 //go:build !windows
 
+// CGO_ENABLED=1 CGO_CFLAGS="$(python3-config --includes)" CGO_LDFLAGS="$(python3-config --ldflags)" go build -v -trimpath -ldflags="-s -w" -buildmode=c-shared -o liner.so
+
 package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/phuslu/log"
+)
 
 /*
 #define Py_LIMITED_API 0x03020000
-
 #include <Python.h>
 
 extern PyObject* liner();
@@ -24,8 +32,27 @@ static struct PyModuleDef mod = {
     methods
 };
 
-PyMODINIT_FUNC PyInit_liner(void) {
+static inline PyObject* PyModule_Create_Liner(void) {
     return PyModule_Create(&mod);
 }
 */
 import "C"
+
+//export liner
+func liner() *C.PyObject {
+	main()
+	C.Py_IncRef(C.Py_None)
+	return C.Py_None
+}
+
+//export linex
+func linex() *C.PyObject {
+	gosh(context.Background(), log.IsTerminal(os.Stdin.Fd()), os.Stdin, os.Stdout, os.Stderr)
+	C.Py_IncRef(C.Py_None)
+	return C.Py_None
+}
+
+//export PyInit_liner
+func PyInit_liner() *C.PyObject {
+	return C.PyModule_Create_Liner()
+}
