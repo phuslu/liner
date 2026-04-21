701b4fbedf151994343e561dc9668577ff9d2a8d
phuslu <phus.lu@gmail.com>
2026-04-22 00:42:17 +0800

    support resolve in http2 tunnel

--- handler_tunnel_http2.go
+++ handler_tunnel_http2.go
@@ -36,7 +36,7 @@ func (h *TunnelHandler) h2tunnel(ctx context.Context, dialerName, dialerURL stri
 		MaxReadFrameSize:   1024 * 1024, // 1MB read frame, https://github.com/golang/go/issues/47840
 		DisableCompression: false,
 		DialTLSContext: func(ctx context.Context, network, addr string, cfg *tls.Config) (net.Conn, error) {
-			hostport := net.JoinHostPort(u.Hostname(), cmp.Or(u.Port(), "443"))
+			hostport := net.JoinHostPort(cmp.Or(u.Query().Get("resolve"), u.Hostname()), cmp.Or(u.Port(), "443"))
 			dialer := h.LocalDialer
 			if md := MemoryDialerOf(ctx, network, hostport); md != nil {
 				dialer = md
