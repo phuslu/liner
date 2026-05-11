f395c5ff5ebf49ed2ddc864e273ff85c54b3455e
phuslu <phus.lu@gmail.com>
2026-05-12 02:39:11 +0800

    packaing liner.cmd to windows tarball

--- build.bash
+++ build.bash
@@ -102,14 +102,14 @@ GOOS=android GOARCH=arm64 \
 
 GOOS=windows GOARCH=amd64 \
 	go build -v -trimpath -ldflags='-s -w -X main.version=1.0.${REVSION}' -o build/liner_windows_amd64/liner.exe && \
-	cp example.yaml liner-ui.exe build/changelog.txt build/liner_windows_amd64/ && \
+	cp example.yaml liner.cmd build/changelog.txt build/liner_windows_amd64/ && \
 	cp wintun/bin/amd64/wintun.dll build/liner_windows_amd64/ && \
 	cd build/liner_windows_amd64 && \
 	tar cv * | gzip -9 >../liner_windows_amd64-${REVSION}.tar.gz
 
 GOOS=windows GOARCH=arm64 \
 	go build -v -trimpath -ldflags='-s -w -X main.version=1.0.${REVSION}' -o build/liner_windows_arm64/liner.exe && \
-	cp example.yaml liner-ui.exe build/changelog.txt build/liner_windows_arm64/ && \
+	cp example.yaml liner.cmd build/changelog.txt build/liner_windows_arm64/ && \
 	cp wintun/bin/arm64/wintun.dll build/liner_windows_amd64/ && \
 	cd build/liner_windows_arm64 && \
 	tar cv * | gzip -9 >../liner_windows_arm64-${REVSION}.tar.gz
