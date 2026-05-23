2133bef919d4605d0875c78df137815556b34408
phuslu <phus.lu@gmail.com>
2026-05-24 02:28:43 +0800

    add server_name to forward log

--- handler_http_forward.go
+++ handler_http_forward.go
@@ -650,6 +650,7 @@ func (h *HTTPForwardHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request
 					Str("logger", "forward").
 					Xid("trace_id", ri.TraceID).
 					NetIPAddrPort("server_addr", ri.ServerAddr).
+					Str("server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
 					Str("tls_server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
 					Str("tls_version", tls.VersionName(ri.TLSVersion)).
 					Str("ja4", ri.JA4).
@@ -778,6 +779,7 @@ func (h *HTTPForwardHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request
 					Str("logger", "forward").
 					Xid("trace_id", ri.TraceID).
 					NetIPAddrPort("server_addr", ri.ServerAddr).
+					Str("server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
 					Str("tls_server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
 					Str("tls_version", tls.VersionName(ri.TLSVersion)).
 					Str("ja4", ri.JA4).
