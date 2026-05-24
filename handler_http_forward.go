a4c6d891cbd88f5a2487ae5d139822b29c3bd398
phuslu <phus.lu@gmail.com>
2026-05-24 20:32:58 +0800

    dont fallback tls server name in forward log +garble +pypi

--- handler_http_forward.go
+++ handler_http_forward.go
@@ -651,7 +651,7 @@ func (h *HTTPForwardHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request
 					Xid("trace_id", ri.TraceID).
 					NetIPAddrPort("server_addr", ri.ServerAddr).
 					Str("server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
-					Str("tls_server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
+					Str("tls_server_name", ri.TLSServerName).
 					Str("tls_version", tls.VersionName(ri.TLSVersion)).
 					Str("ja4", ri.JA4).
 					Str("username", username).
@@ -780,7 +780,7 @@ func (h *HTTPForwardHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request
 					Xid("trace_id", ri.TraceID).
 					NetIPAddrPort("server_addr", ri.ServerAddr).
 					Str("server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
-					Str("tls_server_name", cmp.Or(ri.TLSServerName, ri.ServerAddr.Addr().String())).
+					Str("tls_server_name", ri.TLSServerName).
 					Str("tls_version", tls.VersionName(ri.TLSVersion)).
 					Str("ja4", ri.JA4).
 					Str("username", username).
