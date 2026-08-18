fab36ede81ab1b5c9d1f5dacafb1230e6d933ee9
phuslu <phus.lu@gmail.com>
2026-08-18 15:14:25 +0800

    overwrite GOARCH in macos building

--- build.bash
+++ build.bash
@@ -239,6 +239,14 @@ function liner::python() {
 	case $(uname) in
 		Darwin )
 			command -v python3-config || brew install python
+			case $(uname -m) in
+				x86_64 )
+				export GOARCH=amd64
+				;;
+				aarch64 )
+				export GOARCH=amd64
+				;;
+			esac
 			export CGO_CFLAGS="$(python3-config --includes)"
 			export CGO_LDFLAGS="$(python3-config --ldflags) -undefined dynamic_lookup"
 			export PLATFORM_TAG="macosx_11_0_$(uname -m)"
